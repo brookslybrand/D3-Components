8638154196dc008db4819684b56357c12026b5c5
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2018-12-03 15:13:41 -0600

    First pass at Time Series

--- src/components/TimeSeries/TimeSeries.stories.js
+++ src/components/TimeSeries/TimeSeries.stories.js
@@ -1,24 +1,36 @@
 import React, { useState } from 'react'
+import styled from 'styled-components'
 import { storiesOf } from '@storybook/react';
 import { csv } from 'd3'
 
 import TimeSeries from '.'
 
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+
+  > div {
+    width: 50%
+    height: 80vh
+  }
+`
+
 const DataProvider = ({ children }) => {
   const [data, setData] = useState(() => {
     csv('/data/facebook_stock_12-03-18.csv', (d) => {
-      return {...d, date: new Date(d.date)}
+      const {date, adjClose} = d
+      return [new Date(date), adjClose]
     }).then(data => setData(data))
   })
 
-  return <React.Fragment>
-    {React.cloneElement(children, { data })}
-  </React.Fragment>
+  return React.cloneElement(children, { data })
 }
 
 
 storiesOf('Time Series', module)
-  .addDecorator(story => <div style={{ textAlign: 'center' }}>{story()}</div>)
+  .addDecorator(story => <Container><div>{story()}</div></Container>)
   .add('Time Series', () => <DataProvider>
     <TimeSeries />
   </DataProvider>)
--- src/components/TimeSeries/index.js
+++ src/components/TimeSeries/index.js
@@ -1,12 +1,70 @@
-import React from 'react'
+import React, { useEffect, useLayoutEffect, useRef } from 'react'
+import PropTypes from 'prop-types'
+import * as d3 from 'd3'
 
-const TimeSeries = ({ data }) => {
-  console.log(data)
-  return (
-    <div>
-      {data ? Object.keys(data).length : data}
-    </div>
-  )
+const TimeSeries = ({ data, margin}) => {
+  const svgRef = useRef()
+
+  useEffect(() => {
+    // if there is data, make the time series
+    if (data) {
+      const {width: svgWidth, height: svgHeight} = svgRef.current.getBoundingClientRect()
+
+      const width = Math.floor(svgWidth) - margin.left - margin.right
+      const height = Math.floor(svgHeight) - margin.top - margin.bottom
+
+      const x = d3.scaleTime()
+        .domain(d3.extent(data, d => d[0]))
+        .range([0, width])
+
+      const y = d3.scaleLinear()
+        .domain(d3.extent(data, d => d[1]))
+        .range([height, 0])
+
+      const xAxis = g => g
+        .attr('transform', `translate(0, ${height})`)
+        .call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0))
+
+      const yAxis = g => g
+        .call(d3.axisLeft(y))
+        .call(g => g.select('.domain').remove())
+        // .call(g => g.select('.tick:last-of-type text').clone()
+        //     .attr('x', 3)
+        //     .attr('text-anchor', 'start')
+        //     .attr('font-weight', 'bold')
+        //     .text('test'))
+
+      const line = d3.line()
+          .defined(d => !isNaN(d[1]))
+          .x(d => x(d[0]))
+          .y(d => y(d[1]))
+
+      const svg = d3.select(svgRef.current)
+        .append('g')
+          .attr('transform', `translate(${margin.left},${margin.top})`)
+
+      svg.append('g')
+        .call(xAxis)
+    
+      svg.append('g')
+        .call(yAxis)
+
+      svg.append('path')
+        .datum(data)
+        .attr('fill', 'none')
+        .attr('stroke', 'steelblue')
+        .attr('stroke-width', 1.5)
+        .attr('stroke-linejoin', 'round')
+        .attr('stroke-linecap', 'round')
+        .attr('d', line);
+    }
+  })
+      
+  return <svg ref={svgRef} width='100%' height='100%' />
+}
+
+TimeSeries.defaultProps = {
+  margin: {top: 20, right: 30, bottom: 30, left: 40}
 }
 
 export default TimeSeries
