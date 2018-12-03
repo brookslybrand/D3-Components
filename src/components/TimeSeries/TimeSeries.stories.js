a3e8f20016ef4506840fa780e93f2633dba63044
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2018-12-03 16:08:17 -0600

    Make Time Series adjust with window size change

--- src/components/TimeSeries/TimeSeries.stories.js
+++ src/components/TimeSeries/TimeSeries.stories.js
@@ -17,21 +17,56 @@ const Container = styled.div`
   }
 `
 
+const Button = styled.button`
+
+  &{
+    background-color: white; 
+    border: 2px solid #008CBA;
+    color: black; 
+    padding: 0.8rem 1.4rem;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 1rem 0.8rem;
+    -webkit-transition-duration: 0.4s; /* Safari */
+    transition-duration: 0.4s;
+    cursor: pointer;
+  }
+
+  &:hover {
+      background-color: #008CBA;
+      color: white;
+  }
+
+  &:focus {
+    outline:0;
+  }
+`
+
 const DataProvider = ({ children }) => {
-  const [data, setData] = useState(() => {
+  const [data, setData] = useState()
+
+  const fetchData = () => {
     csv('/data/facebook_stock_12-03-18.csv', (d) => {
       const {date, adjClose} = d
       return [new Date(date), adjClose]
     }).then(data => setData(data))
-  })
+  }
 
-  return React.cloneElement(children, { data })
+  return (<>
+    <Button onClick={fetchData}>Add Data</Button>
+    <Button onClick={() => setData(null)}>Remove Data</Button>
+    {React.cloneElement(children, { data })}
+  </>)
 }
 
 
 storiesOf('Time Series', module)
   .addDecorator(story => <Container><div>{story()}</div></Container>)
-  .add('Time Series', () => <DataProvider>
-    <TimeSeries />
-  </DataProvider>)
+  .add('Single Line', () => (
+    <DataProvider>
+      <TimeSeries />
+    </DataProvider>
+  ))
 
--- src/components/TimeSeries/index.js
+++ src/components/TimeSeries/index.js
@@ -2,61 +2,90 @@ import React, { useEffect, useLayoutEffect, useRef } from 'react'
 import PropTypes from 'prop-types'
 import * as d3 from 'd3'
 
+// TODO: move to another folder
+function removeAllChildren(node) {
+  // as long as a node has a child, remove it
+  // credit: https://stackoverflow.com/questions/683366/remove-all-the-children-dom-elements-in-div
+  while (node.hasChildNodes()) {
+    node.removeChild(node.lastChild);
+  }
+}
+
+const createTimeSeries = function({svgNode, data, margin={}}) {
+  const {width: svgWidth, height: svgHeight} = svgNode.getBoundingClientRect()
+
+  const width = Math.floor(svgWidth) - margin.left - margin.right
+  const height = Math.floor(svgHeight) - margin.top - margin.bottom
+
+  const x = d3.scaleTime()
+    .domain(d3.extent(data, d => d[0]))
+    .range([0, width])
+
+  const y = d3.scaleLinear()
+    .domain(d3.extent(data, d => d[1]))
+    .range([height, 0])
+
+  const xAxis = g => g
+    .attr('transform', `translate(0, ${height})`)
+    .call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0))
+
+  const yAxis = g => g
+    .call(d3.axisLeft(y))
+    // .call(g => g.select('.domain').remove())
+    // .call(g => g.select('.tick:last-of-type text').clone()
+    //     .attr('x', 3)
+    //     .attr('text-anchor', 'start')
+    //     .attr('font-weight', 'bold')
+    //     .text('test'))
+
+  const line = d3.line()
+      .defined(d => !isNaN(d[1]))
+      .x(d => x(d[0]))
+      .y(d => y(d[1]))
+
+  const svg = d3.select(svgNode)
+    .append('g')
+      .attr('transform', `translate(${margin.left},${margin.top})`)
+
+  svg.append('g')
+    .call(xAxis)
+
+  svg.append('g')
+    .call(yAxis)
+
+  svg.append('path')
+    .datum(data)
+    .attr('fill', 'none')
+    .attr('stroke', 'steelblue')
+    .attr('stroke-width', 1.5)
+    .attr('stroke-linejoin', 'round')
+    .attr('stroke-linecap', 'round')
+    .attr('d', line);
+}
+
+const updateTimeSeries = function(svgNode, data, margin) {
+  removeAllChildren(svgNode)
+  createTimeSeries({svgNode, data, margin})
+}
+
 const TimeSeries = ({ data, margin}) => {
   const svgRef = useRef()
 
+  const handleUpdateTimeSeries = () => updateTimeSeries(svgRef.current, data, margin)
+  
   useEffect(() => {
+    const svgNode = svgRef.current
     // if there is data, make the time series
     if (data) {
-      const {width: svgWidth, height: svgHeight} = svgRef.current.getBoundingClientRect()
-
-      const width = Math.floor(svgWidth) - margin.left - margin.right
-      const height = Math.floor(svgHeight) - margin.top - margin.bottom
-
-      const x = d3.scaleTime()
-        .domain(d3.extent(data, d => d[0]))
-        .range([0, width])
-
-      const y = d3.scaleLinear()
-        .domain(d3.extent(data, d => d[1]))
-        .range([height, 0])
-
-      const xAxis = g => g
-        .attr('transform', `translate(0, ${height})`)
-        .call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0))
-
-      const yAxis = g => g
-        .call(d3.axisLeft(y))
-        .call(g => g.select('.domain').remove())
-        // .call(g => g.select('.tick:last-of-type text').clone()
-        //     .attr('x', 3)
-        //     .attr('text-anchor', 'start')
-        //     .attr('font-weight', 'bold')
-        //     .text('test'))
-
-      const line = d3.line()
-          .defined(d => !isNaN(d[1]))
-          .x(d => x(d[0]))
-          .y(d => y(d[1]))
-
-      const svg = d3.select(svgRef.current)
-        .append('g')
-          .attr('transform', `translate(${margin.left},${margin.top})`)
-
-      svg.append('g')
-        .call(xAxis)
-    
-      svg.append('g')
-        .call(yAxis)
-
-      svg.append('path')
-        .datum(data)
-        .attr('fill', 'none')
-        .attr('stroke', 'steelblue')
-        .attr('stroke-width', 1.5)
-        .attr('stroke-linejoin', 'round')
-        .attr('stroke-linecap', 'round')
-        .attr('d', line);
+      createTimeSeries({svgNode, data, margin})
+      window.addEventListener('resize', handleUpdateTimeSeries)
+    // if there is no data, remove anything that might have been added to the svg
+    } else {
+      removeAllChildren(svgNode)
+    }
+    // remove the event listener when things change
+    return () => {
+      window.removeEventListener('resize', handleUpdateTimeSeries)
     }
   })
       
